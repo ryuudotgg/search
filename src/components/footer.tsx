af0ab6f457556016a0e7deadc536984fa760e646
Ryuu <me@ryuu.gg>
2025-05-15 14:58:33 +0400

    feat(yay): add link to personal site

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,15 @@ export function Footer() {
   return (
     <footer className="w-full py-4 text-accent-foreground text-sm text-center absolute bottom-0 left-0 text-muted-foreground">
       <p>
-        &copy; {new Date().getFullYear()} Ryuu
+        &copy; {new Date().getFullYear()}{" "}
+        <a
+          href="https://ryuu.gg"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="transition-colors hover:text-foreground"
+        >
+          Ryuu
+        </a>
         {" • "}
         <Link to="/privacy" className="transition-colors hover:text-foreground">
           Privacy Policy
